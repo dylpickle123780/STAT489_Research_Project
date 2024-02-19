d6aa60f5a15589621c790e80914b42e5b2927556
Dylan Michaels <dmichaels54321@tamu.edu>
2024-02-18 23:02:03 -0600

    Implemented GWR_model function

--- Functions/GWR_implementation.R
+++ Functions/GWR_implementation.R
@@ -1,6 +1,25 @@
 #GWR Implementation 
-library(spgwr)
 
-GWR_model = function(){
+GWR_model = function(data, predictor_variable = price, lon_lat_cols, non_num_cols = state){
+  library(GWmodel)
   
+  #select only numeric data for gwr model
+  data_numeric = data %>% 
+    select(!non_num_cols)
+    
+  #create spatial points data frame using lat and long
+  data_numeric = SpatialPointsDataFrame(coords = data_numeric[,lon_lat_cols],
+                            data = data_numeric[,-lon_lat_cols])
+  
+  
+  model_bandwidth = bw.gwr(predictor_variable ~ ., data = data_no_state,
+                           kernel = "exponential", parallel.method = "omp")
+  
+  #run model with calculated bandwidth and return model
+  model_gwr = gwr.basic(predictor_variable ~ ., data = data_no_state,
+                        bw = model_bandwidth, kernel = "exponential", 
+                        parallel.method = "omp")
+  
+  return(model_gwr)
 }
+
