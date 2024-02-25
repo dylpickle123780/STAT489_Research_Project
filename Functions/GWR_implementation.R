0737c8aa166a7bcebf890a4c89d34aec245dca1a
Dylan Michaels <dmichaels54321@tamu.edu>
2024-02-24 19:14:49 -0600

    GWR model analysis

--- Functions/GWR_implementation.R
+++ Functions/GWR_implementation.R
@@ -1,25 +1,51 @@
 #GWR Implementation 
 
-GWR_model = function(data, predictor_variable = price, lon_lat_cols, non_num_cols = state){
-  library(GWmodel)
+plotting_GWR_predicted = function(model){
+  #turning to plotable data
+  model_results = model$SDF %>% as("sf")
   
-  #select only numeric data for gwr model
-  data_numeric = data %>% 
-    select(!non_num_cols)
-    
-  #create spatial points data frame using lat and long
-  data_numeric = SpatialPointsDataFrame(coords = data_numeric[,lon_lat_cols],
-                            data = data_numeric[,-lon_lat_cols])
-  
-  
-  model_bandwidth = bw.gwr(predictor_variable ~ ., data = data_no_state,
-                           kernel = "exponential", parallel.method = "omp")
-  
-  #run model with calculated bandwidth and return model
-  model_gwr = gwr.basic(predictor_variable ~ ., data = data_no_state,
-                        bw = model_bandwidth, kernel = "exponential", 
-                        parallel.method = "omp")
-  
-  return(model_gwr)
+  model_results %>% 
+    mutate("Predicted Price" = yhat) %>%
+    ggplot() +
+    geom_sf(aes(fill = `Predicted Price`),
+            color = scales::alpha("black",
+                                  alpha = 0.1)) +
+    scale_fill_gradientn(colours = terrain.colors(8)) +
+    theme(text = element_text(size = 20), 
+          legend.position = "bottom") +
+    labs(title = "Predicted Values for Price by GWR w/ exponential kernel")
 }
 
+
+plotting_GWR_residuals = function(model) {
+  model_results = model$SDF %>% as("sf")
+  
+  model_results %>% 
+    mutate("Price Abs Err" = abs(yhat - y)) %>%
+    ggplot() +
+    geom_sf(aes(fill = `Price Abs Err`),
+            color = scales::alpha("black",
+                                  alpha = 0.1)) +
+    scale_fill_viridis_c(limits = c(0, 1000),
+                         breaks = c(250, 500, 750)) +
+    theme(text = element_text(size = 20), 
+          legend.position = "bottom") +
+    labs(title = "Absolute Prediction Error of Price by GWR w/ \nexponential kernel")
+}
+
+plotting_GWR_bedrooms = function(model){
+  model_results = model$SDF %>% as("sf")
+  
+  model_results %>% 
+    mutate("Model variable" = Bedrooms) %>%
+    ggplot() +
+    geom_sf(aes(fill = `Bedrooms`),
+            color = scales::alpha("black",
+                                  alpha = 0.1)) +
+    scale_fill_viridis_c(limits = c(-2000, 600),
+                         breaks = c(-800, 0, 300)) +
+    theme(text = element_text(size = 20), 
+          legend.position = "bottom") +
+    labs(title = "Beta Prediction by GWR w/ \nexponential kernel")
+  
+}
